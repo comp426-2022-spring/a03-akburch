d54a933641b7d987dca5102c4de2886149dd3b33
Anthony Burch <akburch@live.unc.edu>
2022-04-25 13:57:54 -0400

    minor changes

--- server.js
+++ server.js
@@ -7,10 +7,6 @@ const server = app.listen(HTTP_PORT, () => {
     console.log('App listening on port %PORT%'.replace('%PORT%',HTTP_PORT))
 });
 
-app.use(function(req, res){
-    res.status(404).send('404 NOT FOUND')
-});
-
 app.get('/app/', (req, res) => {
     // Respond with status 200
         res.statusCode = 200;
@@ -19,7 +15,9 @@ app.get('/app/', (req, res) => {
         res.writeHead( res.statusCode, { 'Content-Type' : 'text/plain' });
         res.end(res.statusCode+ ' ' +res.statusMessage)
     });
+
 // inserted from a02
+
 function coinFlip() {
     var value = Math.round(Math.random)
     let result = ""
